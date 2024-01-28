0ae8ce3d012cbfff735b8b65dfc2db1ecbdf480e
mariialauraa <mlauraifg@gmail.com>
2024-01-28 18:33:39 -0300

    Add Cadastro de Usuário

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,30 +1,42 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/'
+  baseURL: 'http://localhost:3000/'
 });
 
 const AuthService = {
-    async login(user) {
-        try {
-            const res = await api.post('/admin/v1/login', { user })
-            localStorage.setItem('token', res.data.token)
-            localStorage.setItem('user', JSON.stringify(res.data.user))
-            return res.data            
-        } catch (error) {
-            console.log(error)
-            throw new Error('Usuário ou senha inválidos')
-        }
-    },
+  async login(user) {
+    try {
+      const res = await api.post('/admin/v1/login', { user })
+      localStorage.setItem('token', res.data.token)
+      localStorage.setItem('user', JSON.stringify(res.data.user))
+      return res.data            
+    } catch (error) {
+      console.log(error)
+      throw new Error('Usuário ou senha inválidos')
+    }
+  },
 
-    logout() {
-        localStorage.removeItem('token')
-        localStorage.removeItem('user')
-    },
+  logout() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+  },
 
-    isAuthenticated() {
-        return localStorage.getItem('token') !== null;
-    },
+  isAuthenticated() {
+    return localStorage.getItem('token') !== null;
+  },
+
+  async createAccount(user) {
+    try {
+      const res = await api.post('/admin/v1/users', user); 
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('user', JSON.stringify(res.data.user));  
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Erro ao criar conta'); 
+    }
+  },
 };
 
 export default AuthService;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,6 +9,12 @@ const user = ref({
   password: ''
 })
 
+const newUser = ref({
+  name: '',
+  login: '',
+  password: ''
+})
+
 const login = async () => {
   try {
     await AuthService.login(user.value)
@@ -19,47 +25,133 @@ const login = async () => {
     alert(error.message)
   }
 };
+
+const createAccount = async () => {
+  try {
+    const userData = {
+      name: newUser.value.name,
+      login: newUser.value.login,
+      password: newUser.value.password
+    };
+
+    await AuthService.createAccount({ user: userData });
+    alert('Conta criada com sucesso');
+    resetCreateAccountForm();
+    // router.push('/');
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const resetCreateAccountForm = () => {
+  newUser.value.name = '';
+  newUser.value.login = '';
+  newUser.value.password = '';
+};
 </script>
 
 <template>
-  <VaForm
-    class="w-[300px]"
-    tag="form"
-    @submit.prevent="login"
-  >
-    <VaInput
-      v-model="user.login"
-      label="Login"
-    />
+  <div class="flex flex-col items-center justify-center"> 
+    <img src="/logo-easypallet.png" alt="Easy Pallet Logo" width="300" height="100" class="mt-4 mb-4">
 
-    <VaInput
-      v-model="user.password"
-      class="mt-3"
-      type="password"
-      label="Password"
-    />
-
-    <VaButton
-      type="submit"
-      class="mt-3"
+    <VaForm
+      class="w-full max-w-md"
+      tag="form"
+      @submit.prevent="login"
     >
-      Login
-    </VaButton>
-  </VaForm>
+      <VaInput
+        v-model="user.login"
+        label="E-mail"
+        class="mt-3"
+      />
+
+      <VaInput
+        v-model="user.password"
+        class="mt-3"
+        type="password"
+        label="Senha"
+      />
+
+      <div class="mt-3 flex justify-center">
+        <VaButton
+          type="submit"
+          class="mt-3 mb-3"
+        >
+          Entrar
+        </VaButton>
+      </div>
+    </VaForm>
+
+    <div class="mt-3 flex items-center">
+      <span class="mx-4 text-red-500">ou</span>
+    </div>
+
+    <VaForm
+      class="w-full max-w-md"
+      tag="form"
+      @submit.prevent="createAccount"
+    >
+      <VaInput
+        v-model="newUser.name"
+        label="Nome"
+        class="mt-3"
+      />
+
+      <VaInput
+        v-model="newUser.login"
+        label="E-mail"
+        class="mt-3"
+      />
+
+      <VaInput
+        v-model="newUser.password"        
+        type="password"
+        label="Senha"
+        class="mt-3"
+      />
+
+      <div class="mt-3 flex justify-center">
+        <VaButton
+          type="submit"
+          class="mt-3"
+        >
+          Cadastrar
+        </VaButton>
+      </div>
+    </VaForm>
+  </div>
 </template>
 
 <style scoped>
-.va-input {
-  display: block;
-  width: 30%;
-  margin-left: 20px;
-  margin-top: 20px;
-  margin-bottom: 10px;
+.flex {
+  display: flex;
 }
 
-.va-button {
-  display: block;
-  margin-left: 20px;
+.flex-col {
+  flex-direction: column;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.justify-center {
+  justify-content: center;
+}
+
+.va-form {
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.va-input {
+  width: 100%;
   margin-top: 10px;
+}
+
+.text-red-500 {
+  color: #B50025;
+  font-weight: bold;
 }
 </style>
